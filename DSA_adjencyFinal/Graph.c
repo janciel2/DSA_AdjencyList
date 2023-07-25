2350bb13bb0f9bd50e910935f6738db02b1123d2
janciel2 <119369825+janciel2@users.noreply.github.com>
2023-07-25 11:05:58 +0800

    Update Graph.c

--- DSA_adjencyFinal/Graph.c
+++ DSA_adjencyFinal/Graph.c
@@ -3,6 +3,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<conio.h>
 
 void initDict(Dictionary *dict)
 {
@@ -51,53 +52,69 @@ int hash(STRING vertex)
 
 bool addEdge(Dictionary *dict, STRING vertex, STRING edge)
 {
-	int i = 0, hashValEdge = hash(edge);
-	bool shouldAdd = false, vertexPresent = false;
-	
-	
-	while(i < 26)
-		{
-			if(strcmp(dict[hashValEdge].vertex, edge) == 0)
-				{
-					shouldAdd = true;
-					break;
-				}
-			hashValEdge = (hashValEdge + 1) % MAXSIZE;
-			i++;
-		}
-		
-	i = 0;
-	int hashValVertex = hash(vertex);
-	while(i < 26)
-		{
-			if(strcmp(dict[hashValVertex].vertex, vertex) == 0)
-				{
-					vertexPresent = true;
-					break;
-				}
-			hashValVertex = (hashValVertex + 1) % MAXSIZE;
-			i++;
-		}
-	
-	
-	if(shouldAdd && vertexPresent && (dict[hashValVertex].arrayList.count < 5 && dict[hashValEdge].arrayList.count < 5))
-		{
-			int count = dict[hashValVertex].arrayList.count;
-			strcpy(dict[hashValVertex].arrayList.edge[count],edge);	
-			dict[hashValVertex].arrayList.count++;
-			
-			strcpy(dict[hashValEdge].arrayList.edge[dict[hashValEdge].arrayList.count], dict[hashValVertex].vertex);
-			dict[hashValEdge].arrayList.count++;
-			return true;
-		}
-	else if(dict[hashValVertex].arrayList.count >= 5 || dict[hashValEdge].arrayList.count >= 5)
-		{
-			printf("THE EDGES COUNT REACHED THE MAXIMUM LIMIT");
-		}
-	
-	return false;
-}
+    int i = 0, hashValEdge = hash(edge);
+    bool shouldAdd = false, vertexPresent = false;
 
+    while (i < 26)
+    {
+        if (strcmp(dict[hashValEdge].vertex, edge) == 0)
+        {
+            shouldAdd = true;
+            break;
+        }
+        hashValEdge = (hashValEdge + 1) % MAXSIZE;
+        i++;
+    }
+
+    i = 0;
+    int hashValVertex = hash(vertex);
+    while (i < 26)
+    {
+        if (strcmp(dict[hashValVertex].vertex, vertex) == 0)
+        {
+            vertexPresent = true;
+            break;
+        }
+        hashValVertex = (hashValVertex + 1) % MAXSIZE;
+        i++;
+    }
+
+    if (shouldAdd && vertexPresent && (dict[hashValVertex].arrayList.count < 5 && dict[hashValEdge].arrayList.count < 5))
+    {
+        int count = dict[hashValVertex].arrayList.count;
+        for (i = 0; i < MAX; i++)
+        {
+            if (strcmp(dict[hashValVertex].arrayList.edge[i], edge) == 0 || strcmp(dict[hashValVertex].arrayList.edge[i], "DELETED") == 0)
+            {
+                // Edge is already added or marked as "DELETED," so we won't add it again.
+                return false;
+            }
+            if (strcmp(dict[hashValVertex].arrayList.edge[i], EMPTYSTRING) == 0)
+            {
+                strcpy(dict[hashValVertex].arrayList.edge[i], edge);
+                dict[hashValVertex].arrayList.count++;
+                break;
+            }
+        }
+
+        int v;
+        for (v = 0; v < MAX; v++)
+        {
+            if (strcmp(dict[hashValEdge].arrayList.edge[v], vertex) == 0 || strcmp(dict[hashValEdge].arrayList.edge[v], "DELETED") == 0)
+            {
+                // Edge is already added or marked as "DELETED," so we won't add it again.
+                return false;
+            }
+            if (strcmp(dict[hashValEdge].arrayList.edge[v], EMPTYSTRING) == 0)
+            {
+                strcpy(dict[hashValEdge].arrayList.edge[v], vertex);
+                dict[hashValEdge].arrayList.count++;
+                return true;
+            }
+        }
+    }
+    return false;
+}
 void visualizeDictionary(Dictionary *dict)
 {
 	printf("%5s | %20s\n", "INDEX", "VERTEX");
@@ -122,78 +139,148 @@ void visualizeDictionary(Dictionary *dict)
 
 void displayDictionary(Dictionary *dict)
 {
-	printf("%5s | %20s |%20s\n", "INDEX", "VERTEX", "EDGES");
+	printf("\n\n%5s | %20s |%20s\n", "INDEX", "VERTEX", "EDGES");
 	printf("---------------------------\n");
 	
 	int i;
 	
 	for(i = 0; i < 26; i++)
 		{
-			if(strcmp(dict[i].vertex, EMPTYSTRING) != 0)
+			if(strcmp(dict[i].vertex, EMPTYSTRING) != 0 && strcmp(dict[i].vertex, "DELETED") != 0)
 				{
 					printf("%5d | ", i);
 					printf("%20s", dict[i].vertex);
 						
-						int x = dict[i].arrayList.count, y;
-						for(y = 0; y < x - 1; y++)
+						int y = 0;
+						while(y < MAX)
 							{
-								printf("%15s, ",dict[i].arrayList.edge[y]);
+								if(strcmp(dict[i].arrayList.edge[y], EMPTYSTRING)!= 0)
+									{	
+										printf("%15s, ",dict[i].arrayList.edge[y]);
+									}
+								y++;
 							}
-						printf("%15s ",dict[i].arrayList.edge[y]);		
+						
+						if(strcmp(dict[i].arrayList.edge[y], EMPTYSTRING)!= 0)
+							{
+								printf("%15s ",dict[i].arrayList.edge[y]);			
+							}
 					printf("\n");		
 				}		
 					
 		}
 }
 
-bool deleteEdge(Dictionary *dict, STRING vertex, STRING edge) //delete the edge on the particular vertex and on the Vertex[edge] delete the connection also
+bool deleteEdge(Dictionary *dict, STRING vertex, STRING edge)
 {
-	int i = 0, hashValEdge = hash(edge);
-	bool shouldDelete = false, vertexPresent = false;
-	
-	
-	while(i < 26)
+    int i, hashValEdge = hash(edge);
+    bool shouldDelete = false, vertexPresent = false;
+
+    // Check if the edge is part of the vertex
+    for (i = 0; i < 26; i++)
+    {
+        if (strcmp(dict[hashValEdge].vertex, edge) == 0)
+        {
+            shouldDelete = true;
+            break;
+        }
+        hashValEdge = (hashValEdge + 1) % MAXSIZE;
+    }
+
+    // Check if the vertex is present in the dictionary
+    int hashValVertex = hash(vertex);
+    for (i = 0; i < 26; i++)
+    {
+        if (strcmp(dict[hashValVertex].vertex, vertex) == 0)
+        {
+            vertexPresent = true;
+            break;
+        }
+        hashValVertex = (hashValVertex + 1) % MAXSIZE;
+    }
+
+    if (shouldDelete && vertexPresent) // If both are true
+    {
+        // Delete the edge on the vertex
+        for (i = 0; i < MAX; i++)
+        {
+            if (strcmp(dict[hashValVertex].arrayList.edge[i], edge) == 0)
+            {
+                // Found the edge, mark it as "DELETED"
+                strcpy(dict[hashValVertex].arrayList.edge[i], "DELETED");
+                dict[hashValVertex].arrayList.count--;
+                break;
+            }
+        }
+
+        // Delete the connection on the Vertex[edge]
+        for (i = 0; i < MAX; i++)
+        {
+            if (strcmp(dict[hashValEdge].arrayList.edge[i], vertex) == 0)
+            {
+                // Found the connection, mark it as "DELETED"
+                strcpy(dict[hashValEdge].arrayList.edge[i], "DELETED");
+                dict[hashValEdge].arrayList.count--;
+                break;
+            }
+        }
+        return true;
+    }
+
+    return false;
+}
+
+bool deleteVertex(Dictionary *dict, STRING vertex)
+{
+   	int hashVal = hash(vertex), i = 0;	
+   	bool shouldDelete = false;
+	STRING edgeList[MAX];
+	int edgeCount = 0;	
+	if(strcmp(dict[hashVal].vertex, vertex) == 0)
 		{
-			if(strcmp(dict[hashValEdge].vertex, edge) == 0) //check if the edge is part of the vertex
+			shouldDelete = true;
+		}
+		
+	if(shouldDelete)
+		{
+			int i,v = 0;
+			
+			for(i = 0; i < MAX; i++)
 				{
-					shouldDelete = true;
-					break;
+					if(strcmp(dict[hashVal].arrayList.edge[i], EMPTYSTRING) != 0)
+						{
+							strcpy(edgeList[v], dict[hashVal].arrayList.edge[i]);
+							printf("\n\n\n\n%s", edgeList[v]);
+							v++;
+							edgeCount++;
+						}
 				}
-			hashValEdge = (hashValEdge + 1) % MAXSIZE;
-			i++;
-		}  
+			
+		int y = 0;
+		while(y < edgeCount)
+		{
+			int hashValEdge = hash(edgeList[y]);
+			
+			 for (i = 0; i < MAX; i++) //this part deletes everything inside the edges array;
+        		{
+           			 if (strcmp(dict[hashValEdge].arrayList.edge[i], vertex) == 0)
+            		{
+                		strcpy(dict[hashValEdge].arrayList.edge[i], "DELETED");
+                		dict[hashValEdge].arrayList.count--;
+                		break;
+            		}
+        		}
+			y++;
+		}
+		
+		strcpy(dict[hashVal].vertex, "DELETED");
+		
+		return true;
+		}
 		
 		
-	i = 0;
-	int hashValVertex = hash(vertex);
-	while(i < 26)
-		{
-			if(strcmp(dict[hashValVertex].vertex, vertex) == 0)//check if the vertex is present in the dictionary
-				{
-					vertexPresent = true;
-					break;
-				}
-			hashValVertex = (hashValVertex + 1) % MAXSIZE;
-			i++;
-		}
-	
-	
-	if(shouldDelete && vertexPresent) //if both are true
-		{
-			
-			//delete first the edge on the vertes    
-			int count = dict[hashValVertex].arrayList.count;
-			strcpy(dict[hashValVertex].arrayList.edge[count],edge);	
-			dict[hashValVertex].arrayList.count++;
-			
-			strcpy(dict[hashValEdge].arrayList.edge[dict[hashValEdge].arrayList.count], dict[hashValVertex].vertex);
-			dict[hashValEdge].arrayList.count++;
-			return true;
-		}
-	else if(dict[hashValVertex].arrayList.count >= 5 || dict[hashValEdge].arrayList.count >= 5)
-		{  
-			printf("THE EDGES COUNT REACHED THE MAXIMUM LIMIT");
-		}
+		
+		
 	
 	return false;
 }
