0dfe6ba06d6ae75b365d85732ca1f32e51518179
janciel2 <119369825+janciel2@users.noreply.github.com>
2023-07-25 12:10:13 +0800

    Update main.c

--- DSA_adjencyFinal/main.c
+++ DSA_adjencyFinal/main.c
@@ -16,19 +16,38 @@ int main(int argc, char *argv[])
 	added = addVertex(dict, "Philippines");
 	added = addVertex(dict, "Thailand");
 	added = addVertex(dict, "Mexico");
+	added = addVertex(dict, "Malaysia");
+	added = addVertex(dict, "Korea");
+	added = addVertex(dict, "Japan");
+	
 	
 	visualizeDictionary(dict);
 	displayDictionary(dict);
 	
 	added = addEdge(dict, "Philippines", "Thailand");
-	added = addEdge(dict, "Thailand", "Mexico");
+	added = addEdge(dict, "Philippines", "Mexico");
+	added = addEdge(dict, "Mexico", "Malaysia");
+	added = addEdge(dict, "Malaysia", "Korea");
+	added = addEdge(dict, "Malaysia", "Japan");
+	added = addEdge(dict, "Japan", "Malaysia");
+	added = addEdge(dict, "Japan", "Korea");
+	added = addEdge(dict, "Korea", "Thailand");
+	added = addEdge(dict, "Korea", "Malaysia");
+	added = addEdge(dict, "Thailand", "Korea");
+	added = addEdge(dict, "Thailand", "Philippines");
 	displayDictionary(dict);
 	
 //	deleteEdge(dict, "Philippines", "Thailand");
+//	displayDictionary(dict);
+//	
+//	deleteVertex(dict, "Philippines");
+	
 	displayDictionary(dict);
 	
-	deleteVertex(dict, "Philippines");
+	printf("\n\nDFS:\n\n");
+	DFS(dict);
 	
-	displayDictionary(dict);
+	printf("\n\nBFS:\n\n");
+	BFS(dict);
 	return 0;
 }
